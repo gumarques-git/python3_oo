33705ed5667ca6687c028a406e9498f427f56545
Gus <gumarques@gmail.com>
2019-08-25 01:18:58 +0100

    Testes de Conta em procedural

---
+++ Testes.py
@@ -0,0 +1,14 @@
+# Exercicios. Criando conta e suas Funcoes de maneira procedural
+
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo {}".format(conta["saldo"]))
